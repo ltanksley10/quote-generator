43ecd25cfa4537491a33b2f6ab00477c58b5104f
ltanksley10 <contactlthere@gmail.com>
2021-05-18 19:47:46 +0000

    removed index 0 from state

--- src/App.js
+++ src/App.js
@@ -53,8 +53,8 @@ export default class App extends Component {
         super(props);
         
         this.state = {
-            quote: quotes[0].quote,
-            author: quotes[0].author,
+            quote: quotes.quote,
+            author: quotes.author,
             backgrounds: [
                 photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9, photo10, photo11, photo12, photo13, photo14, photo15, photo16, photo17, photo18,
                 photo19, photo20, photo21, photo22, photo23, photo24, photo25, photo26, photo27, photo28, photo29, photo30, photo31, photo32, photo33, photo34, photo35,
